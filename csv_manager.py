d586c01b904ecfa2b9e53bd9e4870133b0e73d95
davidy22118 <davidy22118@gmail.com>
2020-05-31 19:25:46 +0300

    samer&david r&d py

--- csv_manager.py
+++ csv_manager.py
@@ -14,11 +14,24 @@ class CSV_Manager:
     def to_dict(self, rows):
         keys = rows[0]
         all_data = []
-
-        for row in rows:
+        for row in rows: 
             data_dict = {}
             for i in range(0, len(row)):
                 data_dict[keys[i]] = row[i]
             all_data.append(data_dict)
-
         return all_data
+
+    def to_obj(self, data):
+        
+        all_data={}
+        data.pop(0)
+        for d in data:
+            if( d['author'] in all_data.keys()):
+                all_data[d['author']].append(d)
+            else: all_data[d['author']]=[d]
+        for d in data:
+            if(d['category'] in all_data.keys()):
+                all_data[d['category']].append(d)
+            else : all_data[d['category']]=[d]
+        return all_data
+    
--- main.py
+++ main.py
@@ -6,13 +6,25 @@ def filter_CSV(filter_field, value):
     reader = CSV_Manager("./articles.csv")
     articles = reader.get_csv_as_dicts()
     manipulator = Manipulator(articles)
-
     filtered = manipulator.filter_by(filter_field, value)
     return list(filtered)
+def count_articles(filter_field,value):
+    filtered = filter_CSV(filter_field, value)
+    return len(filtered)
+def is_article(filter_field,value):
+    return count_articles(filter_field,value)>0
+def longest_article(filter_field,value):
+    filtered = filter_CSV(filter_field, value)
+    max = 0
+    for a in filtered:
+        if(int(a['pages'])>max):
+            max = int(a['pages'])
+            article=a
+    return article
 
+    
 
-print("Articles with a title of t4:")
-print(filter_CSV("title", "t4"))
-print('')
-print("Articles of a1 author:")
-print(filter_CSV("author", "a1"))
+reader = CSV_Manager("./articles.csv")
+articles = reader.get_csv_as_dicts()
+
+print(reader.to_obj(articles))
